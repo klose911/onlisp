f8204f82c03076f3a1e74abe74a6ae1552889124
Shanliang Wu <klose@WudeMacBook-Air-2.local>
2016-06-03 23:48:17 +0800

    remove file to function directory

--- application/returnFunc.lisp
+++
@@ -1,9 +0,0 @@
-(defun make-adder (n)
-  #'(lambda (x) (+ x n))) 
-
-;; (setq add3 (make-adder 3)) 
-;; (funcall add3 2) 
-;; (apply add3 '(2)) 
-
-(defun complement (fn)
-  #'(lambda (&rest args) (not (apply fn args))))
