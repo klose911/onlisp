f84d0a2d359cfc18fd6ecbddb4d5dba5f6085492
klose911 <klose911@gmail.com>
2016-06-10 20:36:13 +0800

    add sample for return function by higher order function

--- function/returnFunc.lisp
+++ function/returnFunc.lisp
@@ -7,3 +7,52 @@
 
 (defun complement (fn)
   #'(lambda (&rest args) (not (apply fn args))))
+
+
+(defun our-length (lst)
+  (if (null lst)
+      0
+      (1+ (our-length (cdr lst)))))
+;; (our-length '(2 3 4)) ;; => 3 
+
+(defun our-every (fn lst)
+  (if (null lst)
+      t
+      (and (funcall fn (car lst))
+	   (our-every fn (cdr lst)))))
+
+(defun our-rec (fn lst)
+  (if (null lst)
+      0
+      (funcall fn (our-rec fn (cdr lst))))) 
+
+;;(our-rec #'1+ '(2 3 4)) ;; => 3 
+
+;; rec：一个带2个参数的函数
+;; 第一个参数是：列表的当前car
+;; 第二个参数是：递归的函数
+(defun lrec (rec &optional base)
+  (labels ((self (lst)
+	     (if (null lst)
+		 (if (functionp base)
+		     (funcall base)
+		     base)
+		 (funcall rec (car lst)
+			  #'(lambda ()
+			      (self (cdr lst)))))))
+    #'self))
+
+
+(lrec #'(lambda(x f) (1+ (funcall f))) 0)
+;; apply 需要再包装一层列表
+(apply (lrec #'(lambda(x f) (1+ (funcall f))) 0) '((2 3 4))) ;; 3 
+(funcall (lrec #'(lambda(x f) (1+ (funcall f))) 0) '(2 3 4))  ;; 3 
+
+;; copy-list
+(lrec #'(lambda (x f) (cons x (funcall f))))
+;; remove-duplicates
+(lrec #'(lambda (x f) (adjoin x (funcall f))))
+;; find-if,for some function fn
+(lrec #'(lambda (x f) (if (fn x) x (funcall f))))
+;; some,for some function fn
+(lrec #'(lambda (x f) (or (fn x) (funcall f))))
