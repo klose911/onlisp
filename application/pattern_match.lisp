745c60ced98ea0a153a570dcd6ef511b766fc706
klose911 <klose911@gmail.com>
2016-10-03 21:41:54 +0800

    implement query interpreter

--- application/pattern_match.lisp
+++ application/pattern_match.lisp
@@ -1,3 +1,17 @@
+(defun dbind-ex (binds body)
+  (if (null binds)
+      `(progn ,@body)
+      `(let ,(mapcar #'(lambda (b)
+			 (if (consp (car b))
+			     (car b)
+			     b))
+		     binds)
+	 ,(dbind-ex (mapcan #'(lambda (b)
+				(if (consp (car b))
+				    (cdr b)))
+			    binds)
+		    body)))) 
+
 ;; 遍历匹配模式，将每个变量和运行期对应对象的位置关联在一起
 (defun destruc (pat seq &optional (atom? #'atom) (n 0))
   (if (null pat)
@@ -19,207 +33,112 @@
 				(destruc p var atom?))
 			  rec))))))))
 
-;;(destruc '(?X 'A) 'g #'simple?) ;;=> ((?X (ELT G 0)) ('A (ELT G 1)))
-
-(defun dbind-ex (binds body)
-  (if (null binds)
-      `(progn ,@body)
-      `(let ,(mapcar #'(lambda (b)
-			 (if (consp (car b))
-			     (car b)
-			     b))
-		     binds)
-	 ,(dbind-ex (mapcan #'(lambda (b)
-				(if (consp (car b))
-				    (cdr b)))
-			    binds)
-		    body))))
-
 ;; 第二个参数可以是列表，向量或者它们的任意组合
 (defmacro dbind (pat seq &body body)
   (let ((gseq (gensym)))
     `(let ((,gseq ,seq))
        ,(dbind-ex (destruc pat gseq #'atom) body))))
 
+(defun varsym? (x)
+  (and (symbolp x) (eq (char (symbol-name x) 0) #\?)))
+;; (varsym? 'y) => NIL
+;; (varsym? '?y1) => T
+
+(defmacro aif (test-form then-form &optional else-form)
+  `(let ((it ,test-form))
+     (if it ,then-form ,else-form))) 
+
+;; x:模式变量，如果x存在于binds中，返回x的值以及他相关的绑定
+(defun binding (x binds)
+  (labels ((recbind (x binds)
+	     (aif (assoc x binds)
+		  (or (recbind (cdr it) binds)
+		      it))))
+    (let ((b (recbind x binds)))
+      (values (cdr b) b))))
+;; (binding '?x '((?Y . B) (?X . A))) ;; => A, (?X . A) 
+;; (assoc '?x '((?Y . B) (?X . A)))  ;; =>  (?X . A)
+
+(defmacro acond2 (&rest clauses)
+  (if (null clauses)
+      nil
+      (let ((cl1 (car clauses))
+	    (val (gensym))
+	    (win (gensym)))
+	`(multiple-value-bind (,val ,win) ,(car cl1)
+	   (if (or ,val ,win)
+	       (let ((it ,val)) ,@(cdr cl1))
+	       (acond2 ,@(cdr clauses)))))))
+
+(defun match (x y &optional binds)
+  (acond2
+   ;; x于y值相同，_是通配符 第一个返回值是当前的绑定，第二个返回值是T 
+   ((or (eql x y) (eql x '_) (eql y '_)) (values binds t)) ;; rule 1
+   ;; 模式变量x在当前绑定中能找到，校验y是否是绑定中的值，it的值是(binding x binds)的第一个返回值， 就是当前绑定中变量x绑定的值 
+   ((binding x binds) (match it y binds)) ;; rule 2 -> rule1 
+   ;; 模式变量y在当前绑定中能找到，校验x是否是绑定中的值
+   ((binding y binds) (match x it binds)) ;; rule 3 -> rule1
+   ;; 模式变量x无法在当前绑定找到对应的值，把x和y建立绑定
+   ((varsym? x) (values (cons (cons x y) binds) t)) ;; rule4 -> 执行rule6中(match (cdr x) (cdr y)..
+   ;; 模式变量x无法在当前绑定找到对应的值，把x和y建立绑定
+   ((varsym? y) (values (cons (cons y x) binds) t)) ;; rule5 -> 执行rule6中(match (cdr x) (cdr y).. 
+   ;; 如果(car x) 和 (car y)匹配， 则匹配 (cdr x) (cdr y) 
+   ((and (consp x) (consp y) (match (car x) (car y) binds)) 
+    (match (cdr x) (cdr y) it)) ;; rule6
+   ;; 不匹配
+   (t (values nil nil)))) ;; rule7 
+
+;; 慢的匹配操作符
+(defmacro aif2 (test &optional then else)
+  (let ((win (gensym)))
+    `(multiple-value-bind (it ,win) ,test  
+       (if (or it ,win) ,then ,else))))
+
+;; pat: 模式，模式变量
+;; seq: 序列，具体的值
+;; then: 模式匹配成功后的执行语句
+;; else: 模式匹配失败后的执行语句
+(defmacro if-match (pat seq then &optional else)
+  ;; 通过比较模式跟序列来建立绑定 
+  `(aif2 (match ',pat ,seq)
+	 ;; 把then表达是出现的模式变量列表替换成序列中对应绑定的值
+	 (let ,(mapcar #'(lambda (v)
+			   `(,v (binding ',v it)))
+		       (vars-in then #'atom))
+	   ,then)
+	 ,else))
+
+;; 获取一个表达式中在出现的模式变量列表  
 (defun vars-in (expr &optional (atom? #'atom))
   (if (funcall atom? expr)
       (if (var? expr) (list expr))
       (union (vars-in (car expr) atom?)
 	     (vars-in (cdr expr) atom?))))
-
 (defun var? (x)
   (and (symbolp x) (eq (char (symbol-name x) 0) #\?)))
 
-(defun gensym? (s)
-  (and (symbolp s) (not (symbol-package s))))
+;; (defun abab (seq)
+;;   (if-match (?x ?y ?x ?y) seq
+;; 	    (values ?x ?y)
+;; 	    nil))
+;; (abab '(hi ho hi ho)) ;; => HI, HO
 
-(defun length-test (pat rest)
-  (let ((fin (caadar (last rest))))
-    (if (or (consp fin) (eq fin 'elt))
-	`(= (length ,pat) ,(length rest))
-	`(> (length ,pat) ,(- (length rest) 2)))))
+;; (match '(?x ?y ?x ?y) '(hi ho hi ho)) ;; => ((?Y . HO) (?X . HI)), T
 
-(defun match1 (refs then else)
-  (dbind ((pat expr) . rest) refs
-	 (cond ((gensym? pat) ;;如果模式参数是一个生成符号，那么它是一个由 destruc 创建用于保存子列表的不可见变量，并且所有我们需要在运行期做的就是测试它是否具有正确的长度
-		`(let ((,pat ,expr))
-		   (if (and (typep ,pat 'sequence)
-			    ,(length-test pat rest))
-		       ,then
-		       ,else)))
-	       ((eq pat '_) then) ;; 通配符不需要做任何的事情
-	       ((var? pat) ;; 
-		(let ((ge (gensym)))
-		  `(let ((,ge ,expr))
-		     ;; 模式变量还未赋值或者模式变量已经和表达式中对应的值相同
-		     (if (or (gensym? ,pat) (equal ,pat ,ge))
-			 ;; 模式变量绑定为表达式的值
-			 (let ((,pat ,ge)) ,then)
-			 ,else))))
-	       (t `(if (equal ,pat ,expr) ;; 一个字面上的值，生成代码去比较它和序列中的对应部分
-		       ,then ,else)))))
+;; (vars-in '(values ?x ?y)) ;; => (?X ?Y)
+
+;; (mapcar #'(lambda (v)
+;; 	    `(,v (binding ',v '((?Y . HO) (?X . HI)))))
+;;         (vars-in '(values ?x ?y) #'atom))
+;; => ((?X (BINDING '?X '((?Y . HO) (?X . HI)))) (?Y (BINDING '?Y '((?Y . HO) (?X . HI)))))
+
+;; (macroexpand-1 '(if-match (?x ?y ?x ?y) seq
+;; 		 (values ?x ?y)
+;; 		 nil))
+;; => (AIF2 (MATCH '(?X ?Y ?X ?Y) SEQ)
+;; 	 (LET ((?X (BINDING '?X IT))
+;; 	       (?Y (BINDING '?Y IT)))
+;; 	   (VALUES ?X ?Y))
+;; 	 NIL)
 
 
-;; pat: 'A, expr: (ELT G 1)  , rest: NIL
-;; pat是常量，调用规则4 
-;;(match1 '(('A (ELT G 1))) '(print ?x) nil) 
-;; => (IF (EQUAL 'A (ELT G 1)) (PRINT ?X) NIL)
-
-;; pat: ?X expr: (ELT G 0), rest: NIL
-;; pat是模式变量，调用规则3， ?X 绑定到对应的表达式(ELT G 0) 
-;; (match1 '((?X (ELT G 0)))
-;; 	'(IF (EQUAL 'A (ELT G 1)) (PRINT ?X) NIL) NIL) 
-;; (LET ((#:G3494 (ELT G 0)))
-;;   (IF (OR (GENSYM? ?X)
-;; 	  (EQUAL ?X #:G3494))
-;;       (LET ((?X #:G3494))
-;; 	(IF (EQUAL 'A (ELT G 1))
-;; 	    (PRINT ?X) NIL)) NIL))
-
-(defmacro with-gensyms-1 (syms &body body)
-  `(let ,(mapcar #'(lambda (s)
-                     `(,s (gensym)))
-                 syms)
-     ,@body))
-
-(defun simple? (x) (or (atom x) (eq (car x) 'quote)))
-;; (simple? 'a) ;; => T
-;; (simple? (quote s)) ;; T 
-;; (simple? '(a b)) ;; => NIL
-
-(defun gen-match (refs then else)
-  (if (null refs)
-      then
-      (let ((then (gen-match (cdr refs) then else)))
-	(if (simple? (caar refs))
-	    (match1 refs then else)
-	    (gen-match (car refs) then else)))))
-
-;; (gen-match '((G SEQ) ((?X (ELT G 0)) ('A (ELT G 1)))) '(print ?X)) ;; 调用点a  
-;; 一路递归到列表的结尾，然后在回来的路上构造其返回值
-
-;; (let ((then (gen-match '(((?X (ELT G 0)) ('A (ELT G 1)))) '(print ?X))) ;; 调用点b 
-;;     ....
-
-;; (let ((then (gen-match '(('A (ELT G 1)))) '(print ?X))) ;; 调用点c
-;;     ....
-
-;; (let ((then (gen-match nil '(print ?X))))
-;; gen-match走完所有元素，then就是(print ?x) , 继续执行调用点c
-
-;; (simple? (caar '(('A (ELT G 1)))) => T 
-;; (match1 '(('A (ELT G 1))) '(print ?x) nil)
-;; => (IF (EQUAL 'A (ELT G 1)) (PRINT ?X) NIL) 作为返回值then，继续调用点b
-
-;; (gen-match '(((?X (ELT G 0)) ('A (ELT G 1)))) '(IF (EQUAL 'A (ELT G 1)) (PRINT ?X) NIL)) 
-;; (simple? (caar '(((?X (ELT G 0)) ('A (ELT G 1)))))) => NIL
-;; (gen-match '((?X (ELT G 0)) '(IF (EQUAL 'A (ELT G 1)) (PRINT ?X) NIL))
-;; (simple? (caar '((?X (ELT G 0))))) => T
-;; (match1 '((?X (ELT G 0))) '(IF (EQUAL 'A (ELT G 1)) (PRINT ?X) NIL) NIL)
-;; => (LET ((#:G3494 (ELT G 0)))
-;;   (IF (OR (GENSYM? ?X)
-;; 	  (EQUAL ?X #:G3494))
-;;       (LET ((?X #:G3494))
-;; 	(IF (EQUAL 'A (ELT G 1))
-;; 	    (PRINT ?X) NIL)) NIL)) 作为返回值，继续调用点a
-
-;; (simple? (caar '((G SEQ) ((?X (ELT G 0)) ('A (ELT G 1)))))) => T
-;; (match1 '((G SEQ) ((?X (ELT G 0)) ('A (ELT G 1)))) '(LET ((#:G3494 (ELT G 0)))
-;;   (IF (OR (GENSYM? ?X)
-;; 	  (EQUAL ?X #:G3494))
-;;       (LET ((?X #:G3494))
-;; 	(IF (EQUAL 'A (ELT G 1))
-;; 	    (PRINT ?X) NIL)) NIL)) 
-
-;; (match1 '(G SEQ)  '(LET ((#:G3494 (ELT G 0)))
-;;   (IF (OR (GENSYM? ?X)
-;; 	  (EQUAL ?X #:G3494))
-;;       (LET ((?X #:G3494))
-;; 	(IF (EQUAL 'A (ELT G 1))
-;; 	    (PRINT ?X) NIL)) NIL))
-;; g是一个生成的symbol，调用规则1, 进行长度校验
-
-(defmacro pat-match (pat seq then else)
-  (if (simple? pat)
-      (match1 `((,pat ,seq)) then else)
-      (with-gensyms-1 (gseq gelse)
-	`(labels ((,gelse () ,else))
-	   ,(gen-match (cons (list gseq seq)
-			     (destruc pat gseq #'simple?))
-		       then
-		       `(,gelse))))))
-;; (gen-match  '((G SEQ) ((?X (ELT G 0)) ('A (ELT G 1))))  '(print ?X)) 
-
-(defmacro if-match-fast (pat seq then &optional else)
-  ;; 为每个模式变量分配symbol 
-  `(let ,(mapcar #'(lambda (v) `(,v ',(gensym)))
-		 (vars-in pat #'simple?))
-     (pat-match ,pat ,seq ,then ,else)))
-
-(setq seq '(b a)) 
-(if-match-fast (?x 'a) seq 
-	  (print ?x)) ;; => B 
-;; (macroexpand-1  '(if-match-fast (?x 'a) seq 
-;; 		 (print ?x)))
-;; (LET ((?X '#:G3496))
-;;   (PAT-MATCH (?X 'A) SEQ
-;; 	     (PRINT ?X) NIL))
-
-;; 完整的if-match展开式 
-;; (let ((?x '#:g1)) ;; 而变量?x在开始的被绑定到了一个 gensym，以表明它尚未被匹配操作赋给一个值。
-;;   (labels ((#:g3 nil nil)) 
-;;      match1的规则1：校验模式长度是否和表达式长度一致
-;;     (let ((#:g2 seq)) ;;这些变量的名字是用gensym生成的，目的是为了避免捕捉
-;;       (if (and (typep #:g2 'sequence)
-;;           (= (length #:g2) 2))
-;;         match1的规则3：？X未赋值，模式变量赋值为表达式的第一个元素
-;;         (let ((#:g5 (elt #:g2 0))) 
-;;           (if (or (gensym? ?x) (equal ?x #:g5))
-;;             (let ((?x #:g5))
-;;              match1的规则4： 校验模式的第二个元素是否与表达式的第二个元素相同
-;;               (if (equal 'a (elt #:g2 1))
-;;                 (print ?x)
-;;                 (#:g3)))
-;;             (#:g3)))
-;;         (#:g3)))))
-
-;; 在新的 if-match 中，模式元素现在是被求值而不再是被隐式引用了。这意味着Lisp变量可以被用于模式中，和被引用的表达式一样：
-(let ((n 3))
-  (if-match-fast (?x n 'n '(a b)) '(1 3 n (a b))
-		 ?x)) ;; => 1
-
-;; 还有两个进一步的改进，是因为新版本调用了 destruc 而出现。现在模式中可以包含 &rest 或者 &body
-;; 并且因为 destruc 使用了一般的序列操作符 elt 和 subseq, 将工作在任何类型的序列上
-
-(defun abab (seq)
-  (if-match-fast (?x ?y ?x ?y) seq
-    (values ?x ?y)
-    nil))
-
-;; 字符串
-(abab "abab") ;; => #\a, #\b 
-;; 向量 
-(abab #(1 2 1 2)) ;; => 1, 2 
-
-(if-match-fast (?x (1 . ?y) . ?x) '((a b) #(1 2 3) a b)
-  (values ?x ?y)) ;; => (A B), #(2 3) 
--- application/query.lisp
+++ application/query.lisp
@@ -1,3 +1,176 @@
 ;; 19 查询编译器
 ;; 嵌入式语言: 用某种现有的语言实现的, 实现的方式通常是采用转换, 在Lisp里使用宏进行转换
 (load "pattern_match.lisp" :external-format charset:utf-8) 
+
+;; 19.1 数据库
+;; (painter reynolds joshua english) ;; Joshua Reynolds是一位的英国画家
+;; (dates reynolds 1723 1792) ;; Joshua Reynolds生活于1723至1792年
+
+;; 数据库由一张哈希表表示，表项则是一个个事实，事实的谓词作为哈希表的键值
+(defun make-db (&optional (size 100))
+  (make-hash-table :size size))
+
+(defvar *default-db* (make-db))
+
+
+;; 清空当前数据库 
+(defun clear-db (&optional (db *default-db*))
+  (clrhash db))
+
+;; 根据谓词来查询哈希表
+(defmacro db-query (key &optional (db '*default-db*))
+  `(gethash ,key ,db))
+
+;; 哈希表的对应key的列表中插入val
+(defun db-push (key val &optional (db *default-db*))
+  (push val (db-query key db)))
+
+;; 给数据库加入新事实。
+(defmacro fact (pred &rest args)
+  `(progn (db-push ',pred ',args)
+	  ',args))
+
+(clear-db) 
+*default-db* ;; => #S(HASH-TABLE :TEST FASTHASH-EQL)
+(fact painter reynolds joshua english) ;; => (REYNOLDS JOSHUA ENGLISH)
+*default-db* ;; => #S(HASH-TABLE :TEST FASTHASH-EQL (PAINTER . ((REYNOLDS JOSHUA ENGLISH))))
+(fact painter canale antonio venetian) ;; => (CANALE ANTONIO VENETIAN)
+*default-db* ;; =>  #S(HASH-TABLE :TEST FASTHASH-EQL (PAINTER . ((CANALE ANTONIO VENETIAN) (REYNOLDS JOSHUA ENGLISH))))
+(db-query 'painter) ;; => ((CANALE ANTONIO VENETIAN) (REYNOLDS JOSHUA ENGLISH)), T
+
+;; 19.2 模式匹配查询
+;; db-query查询数据库中的数据这种方式不是很灵活
+;; 查询语言就是一种用来表达更复杂查询的语言
+
+;; 查询所有出生1697年的画家
+;;  (and (painter ?x ?y ?z)
+;;     (dates ?x 1697 ?w))　
+
+;; 查询语法
+;; <query> : (<symbol> <argument>*)
+;;         : (not <query>)
+;;         : (and <query>*)
+;;         : (or <query>*)
+
+;; <argument> : ?<symbol>
+;;            : <symbol>
+;;            : <number>　
+
+;; 对于真值采取怀疑论的观点：除了数据库中存在的记录，其他都认为是假　
+
+;; 查询解释器接受查询，并根据它从数据库里生成答案
+;; 查询编译器接受查询，然后生成一个程序，当这个程序运行时，从数据库里生成答案
+
+;; 19.3 查询解释器
+(clear-db)
+(fact painter hogarth william english)
+(fact painter canale antonio venetian)
+(fact painter reynolds joshua english)
+(fact dates hogarth 1697 1772)
+(fact dates canale 1697 1768)
+(fact dates reynolds 1723 1792)
+
+;; 宏with-answer提供了使用这个查询解释器的清爽简洁的接口方法
+;; 第一个参数query是任意合法的查询
+;; 其余参数body是一个代码体
+(defmacro with-answer (query &body body)
+  (let ((binds (gensym)))
+    ;; dolist not work !!!!
+    `(dolist (,binds (interpret-query ',query)) ;; 收集由查询生成的所有绑定的集合
+       (let ,(mapcar #'(lambda (v)
+			 `(,v (binding ',v ,binds))) ;; 每个绑定集合所指定的变量来迭代整个代码体
+		     (vars-in query #'atom)) ;; 当查询成功但却不含有变量时，只求值代码体一次。
+	 ,@body))))
+
+;; (vars-in '(or (dates ?x ?y 1772)
+;; 	   (dates ?x ?y 1792) #'atom)) ;; (?x ?y)
+
+;; (macroexpand-1 '(with-answer (or (dates ?x ?y 1772)
+;;     (dates ?x ?y 1792))
+;; 		 (princ (list ?x ?y))))
+;; => 
+;; (DOLIST (#:G3547 (INTERPRET-QUERY '(OR (DATES ?X ?Y 1772) (DATES ?X ?Y 1792))))
+;;   (LET ((?X (BINDING '?X #:G3547))
+;; 	(?Y (BINDING '?Y #:G3547)))
+;;     (PRINC (LIST ?X ?Y))))
+
+;; (INTERPRET-QUERY '(OR (DATES ?X ?Y 1772) (DATES ?X ?Y 1792))) 
+;; => (((?Y . 1697) (?X . HOGARTH)) ((?Y . 1723) (?X . REYNOLDS)))
+
+;; (let ((?X (binding '?X '((?Y . 1697) (?X . HOGARTH))))
+;;       (?Y (binding '?Y '((?Y . 1697) (?X . HOGARTH)))))
+;;   (PRINC (LIST ?X ?Y)))
+;; => (HOGARTH 1697) 
+
+;; 递归地对复杂查询的数据结构进行处理，在这个过程中生成绑定
+;; 复杂查询的求值按从左到右的顺序进行
+;; 结果会根据逻辑操作符或被滤除，或被组合
+(defun interpret-query (expr &optional binds)
+  (case (car expr)
+    (and (interpret-and (reverse (cdr expr)) binds))
+    (or (interpret-or (cdr expr) binds))
+    (not (interpret-not (cadr expr) binds))
+    (t (lookup (car expr) (cdr expr) binds))))
+
+
+;; (interpret-query '(and (painter ?x ?y ?z)
+;; 			(dates ?x 1697 ?w)))  　
+;; => (((?W . 1768) (?Z . VENETIAN) (?Y . ANTONIO) (?X . CANALE)) ((?W . 1772) (?Z . ENGLISH) (?Y . WILLIAM) (?X . HOGARTH)))
+
+;; (car '(and (painter ?x ?y ?z)
+;;        (dates ?x 1697 ?w)))  ;; => AND
+;; (reverse (cdr '(and (painter ?x ?y ?z)
+;; 	   (dates ?x 1697 ?w)))) 
+;; => ((DATES ?X 1697 ?W) (PAINTER ?X ?Y ?Z))
+
+　
+(defun interpret-and (clauses binds)
+  (if (null clauses)
+      (list binds)
+      (mapcan #'(lambda (b)
+		  (interpret-query (car clauses) b))
+	      (interpret-and (cdr clauses) binds))))
+
+;; (interpret-and
+;;  '((DATES ?X 1697 ?W) (PAINTER ?X ?Y ?Z))
+;;  '())
+
+(defun interpret-or (clauses binds)
+  (mapcan #'(lambda (c)
+	      (interpret-query c binds))
+	  clauses))
+
+(defun interpret-not (clause binds)
+  (if (interpret-query clause binds)
+      nil
+      (list binds)))
+
+;; 函数lookup接受一个由谓词及其参数列表所组成的模式
+;; 返回一个能够使模式匹配到数据库中某个事实的所有绑定的列表
+(defun lookup (pred args &optional binds)
+  ;; lookup返回一个含有所有这些列表的列表
+  (mapcan #'(lambda (x)
+	      ;; 调用match把它们和模式逐一比较。每当匹配成功，就返回一个绑定列表
+	      (aif2 (match x args binds) (list it)))
+	  ;; 获取所有该谓词的数据库表项 
+	  (db-query pred)))
+
+;;(db-query 'painter)
+;; => ((CANALE ANTONIO VENETIAN) (REYNOLDS JOSHUA ENGLISH))
+
+;; (match '(REYNOLDS JOSHUA ENGLISH) '(?x ?y english) '((?z 1668)))  
+;;   => ((?Y . JOSHUA) (?X . REYNOLDS) (?Z 1668)), T 
+
+;; (mapcan #'(lambda (x)
+;; 	    (aif2 (match x '(?x ?y english) '((?z 1668))) (list it)))
+;; 	'((CANALE ANTONIO VENETIAN) (REYNOLDS JOSHUA ENGLISH) (ALEX NEWTON ENGLISH)))
+;; => (((?Y . JOSHUA) (?X . REYNOLDS) (?Z 1668)) ((?Y . NEWTON) (?X . ALEX) (?Z 1668)))
+
+;; (lookup 'painter '(?x ?y english)) 
+;; => (((?Y . JOSHUA) (?X . REYNOLDS)))
+
+;; (with-answer (and (dates ?x ?b ?d)
+;;    (lisp (> (- ?d ?b) 70)))
+;;   (format t "~A lived over 70 years.~%" ?x))
+
+;; 19.5 查询编译器
